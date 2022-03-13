e51a7f1d6c7602aae52abfeab6732d8debc9e4e5
nibrahim2 <nuratuibrahim3@gmail.com>
2022-03-13 06:04:22 -0700

    error fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* main --entry point */
+/**
+ * main -entry point for the code block
+ * Return: returns 0 after the program has been compiled
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
