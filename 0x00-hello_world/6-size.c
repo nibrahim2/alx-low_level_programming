e10e97fe67f1ba0ec3d586eea33fada71b0c5c86
nibrahim2 <nuratuibrahim3@gmail.com>
2022-03-13 06:53:02 -0700

    error fixed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -entry point to the main block
+ * Return: returns 0 at the end of compilation
+ */
+int main(void)
+{
+	printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("Size of a int:  %zu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
+	return (0);
+}
