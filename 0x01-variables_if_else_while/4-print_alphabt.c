72220346ab46fca560ac12d50aeac9a96a12f205
nibrahim2 <nuratuibrahim3@gmail.com>
2022-03-14 22:07:02 -0700

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -entry point to the code block
+ * Return: returns 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
