ec4a6147d75a5f0eec0d3e04a9498eaf926503ad
nibrahim2 <nuratuibrahim3@gmail.com>
2022-03-14 22:12:07 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -entry point to the code block
+ * Return: returns 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
+
